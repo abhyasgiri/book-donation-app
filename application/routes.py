53b9454b6c913cb3a5f3cfc8c4e5d568a3d4ddf9
Abhyas Giri <abhyasgiri1@gmail.com>
2020-12-29 21:29:34 +0000

    changes

--- application/routes.py
+++ application/routes.py
@@ -21,23 +21,17 @@ def create():
             return redirect(url_for("home"))
     return render_template ("add.html", title="Create a book", form=form)
 
-@app.route("/watchlist/<int:id>")            #to set a task to do as complete
-def watchlist(id):
-    book = Books.query.filter_by(id=id).first()
-    book.watchlisted = True
-    db.session.commit()
-    return f"Book {id} has been added to the watchlist"
-
 @app.route("/update/<int:id>", methods = ["GET", "POST"])              #to update something on the to do list
-def update(id):
+def updateshop(id):
     form = Form()
-    book = Books.query.filter_by(id=id).first()
+    shop = Shops.query.filter_by(id=id).first()
     if request.method == "POST":
-        book.description = form.description.data
+        shop.shop_name = form.shop_name.data
+        shop.location = form.location.data
         db.session.commit()
         return redirect(url_for("home"))
 
-    return render_template("update.html", form=form, title="Update Book", book=book)
+    return render_template("update.html", form=form, title="Update Shop", shop=shop)
 
 
 @app.route("/delete/<int:id>", methods = ["GET", "POST"])
